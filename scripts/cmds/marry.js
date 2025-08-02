ee1c3ad6cf50f3eace4dccf197899a6a86e8d362
Rahad <merrybawm998@gmail.com>
2025-08-02 22:00:49 +0600

    Update marry.js

--- scripts/cmds/marry.js
+++ scripts/cmds/marry.js
@@ -6,14 +6,14 @@ const fs = require("fs")
 module.exports = {
     config: {
         name: "marry",
-        aliases: ["marryv4","marryfour"],
+        aliases: [],
         version: "1.0",
         author: "\x4c\x45\x41\x52\x4e\x20\x54\x4f\x20\x45\x41\x54\x20\x4c\x45\x41\x52\x4e\x20\x54\x4f\x20\x53\x50\x45\x41\x4b\x20\x42\x55\x54\x20\x44\x4f\x4e\'\x54\x20\x54\x52\x59\x20\x54\x4f\x20\x43\x48\x41\x4e\x47\x45\x20\x54\x48\x45\x20\x43\x52\x45\x44\x49\x54\x20\x41\x4b\x41\x53\x48",//don't change credit otherwise I'm gonna fuck your mom
         countDown: 5,
         role: 0,
         shortDescription: "get a wife",
         longDescription: "mention your love❗",
-        category: "love",
+        category: "fun",
         guide: "{pn}"
     },
 
@@ -47,4 +47,4 @@ async function bal(one, two) {//credit akash #_#
 
     await img.writeAsync(pth)
     return pth
-}
+                                                        }
