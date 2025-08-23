b9ef6e2f5ce366350f7b5907a2816f8cfaa8ef83
Rahad <merrybawm998@gmail.com>
2025-08-23 20:36:53 +0600

    Update fuck.js

--- scripts/cmds/fuck.js
+++ scripts/cmds/fuck.js
@@ -1,50 +1,94 @@
-const axios = require('axios');
-const jimp = require("jimp");
-const fs = require("fs")
-
+// fingering.js
+const fs = require("fs-extra");
+const Canvas = require("canvas");
+const path = require("path");
 
 module.exports = {
- config: {
- name: "fuck",
- aliases: ["chikai","cdn"],
- version: "1.0",
- author: "MR.AYAN",
- countDown: 5,
- role: 0,
- shortDescription: "",
- longDescription: "",
- category: "18+",
- guide: "{pn}"
- },
+  config: {
+    name: "fuck",
+    aliases: ["chuda", "chod"],
+    version: "1.0.5",
+    author: "RAHAD",
+    countDown: 5,
+    role: 0,
+    shortDescription: "Crush edit with custom background",
+    longDescription: "Put user profile pictures on custom background",
+    category: "funny",
+    guide: "{pn} @tag"
+  },
 
+  onLoad: async () => {
+    const dirMaterial = path.join(__dirname, "cache");
+    const bgPath = path.join(dirMaterial, "fingering_new.png");
+    const bgUrl = "https://drive.google.com/uc?export=download&id=1-St_iO7eEDBPxIfpnmR4cT9BaBhnkDP9";
 
+    if (!fs.existsSync(dirMaterial)) fs.mkdirSync(dirMaterial, { recursive: true });
+    if (!fs.existsSync(bgPath)) {
+      const { downloadFile } = global.utils;
+      await downloadFile(bgUrl, bgPath);
+    }
+  },
 
- onStart: async function ({ message, event, args }) {
- const mention = Object.keys(event.mentions);
- if (mention.length == 0) return message.reply("Please mention someone");
- else if (mention.length == 1) {
- const one = event.senderID, two = mention[0];
- bal(one, two).then(ptth => { message.reply({ body: "💦𝗢𝗙𝗦𝗦 𝗧𝗛𝗜𝗦 𝗜𝗦 𝗙𝗢𝗥 𝗬𝗢𝗨💦", attachment: fs.createReadStream(ptth) }) })
- } else {
- const one = mention[1], two = mention[0];
- bal(one, two).then(ptth => { message.reply({ body: "", attachment: fs.createReadStream(ptth) }) })
- }
- }
+  onStart: async function ({ event, api }) {
+    try {
+      const { senderID, mentions, threadID, messageID } = event;
+      const mention = Object.keys(mentions || {});
+      if (!mention[0]) return api.sendMessage("❌ | Please mention someone!", threadID, messageID);
 
+      const id1 = senderID;
+      const id2 = mention[0];
 
-};
+      const bgPath = path.join(__dirname, "cache/fingering_new.png");
+      const background = await Canvas.loadImage(bgPath);
 
-async function bal(one, two) {
+      // Create canvas
+      const canvas = Canvas.createCanvas(background.width, background.height);
+      const ctx = canvas.getContext("2d");
 
- let avone = await jimp.read(`https://graph.facebook.com/${one}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)
- avone.circle()
- let avtwo = await jimp.read(`https://graph.facebook.com/${two}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)
- avtwo.circle()
- let pth = "fucked.png"
- let img = await jimp.read("https://i.ibb.co/YpR7Bpv/image.jpg")
+      // Draw background
+      ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
 
- img.resize(639,  480).composite(avone.resize(90, 90), 23, 320).composite(avtwo.resize(100, 100), 110, 60);
+      // Load avatars
+      const avatar1 = await Canvas.loadImage(
+        `https://graph.facebook.com/${id1}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`
+      );
+      const avatar2 = await Canvas.loadImage(
+        `https://graph.facebook.com/${id2}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`
+      );
 
- await img.writeAsync(pth)
- return pth
+      // Draw avatars with **same size and positions as fuck.js**
+      const left = { x: 190, y: 200, size: 180 };
+      const right = { x: 390, y: 200, size: 180 };
+
+      function drawCircle(img, x, y, size) {
+        ctx.save();
+        ctx.beginPath();
+        ctx.arc(x + size / 2, y + size / 2, size / 2, 0, Math.PI * 2, true);
+        ctx.closePath();
+        ctx.clip();
+        ctx.drawImage(img, x, y, size, size);
+        ctx.restore();
+      }
+
+      drawCircle(avatar1, left.x, left.y, left.size);
+      drawCircle(avatar2, right.x, right.y, right.size);
+
+      // Save & send
+      const pathOut = path.join(__dirname, "cache/fingering_out.png");
+      const out = fs.createWriteStream(pathOut);
+      const stream = canvas.createPNGStream();
+      stream.pipe(out);
+      out.on("finish", () => {
+        api.sendMessage(
+          { body: "", attachment: fs.createReadStream(pathOut) },
+          threadID,
+          () => fs.unlinkSync(pathOut),
+          messageID
+        );
+      });
+    } catch (err) {
+      console.error(err);
+      api.sendMessage("❌ | Something went wrong!", event.threadID, event.messageID);
+    }
   }
+};
